3de0c9b24bcc79c520d3c295d8dd8c59fb1c0add
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2023-12-20 09:15:47 -0300

    Create small project to run migrations when deployong

---
+++ MigrationTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using AnimeList.Data;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Applying database migrations...");
+
+var optionsBuilder = new DbContextOptionsBuilder<AnimeDbContext>();
+optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Pooling=true;Database=ANIME_LIBRARY;User Id=postgres;Password=1234");
+
+using var context = new AnimeDbContext(optionsBuilder.Options);
+context.Database.Migrate();
+
+Console.WriteLine("Migrations applied successfully.");
