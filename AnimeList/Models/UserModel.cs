14b91e63beffe96b9a27c07a0af4cd81c070f80b
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2023-12-30 03:06:51 -0300

    Add model and migrations for user and register models

---
+++ AnimeList/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimeList.Models
+{
+    [Table("User")]
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public byte[] Salt { get; set; }
+    }
+}
