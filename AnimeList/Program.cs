9becd695411016ce523b1789911f921c08aee75c
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-02 03:01:17 -0300

    Configure Swagger to handle JWT authenticated requests

--- AnimeList/Program.cs
+++ AnimeList/Program.cs
@@ -8,7 +8,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +22,8 @@ builder.Services.AddSingleton<TokenService>();
 builder.Services.AddScoped<AnimeService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddAutoMapper(typeof(AnimeProfile));
-var config = new MapperConfiguration(cfg => {
+var config = new MapperConfiguration(cfg =>
+{
     cfg.CreateMap<BaseAnimeModel, BaseAnimeModelDTO>();
 });
 
@@ -28,7 +33,41 @@ builder.Services.AddDbContext<AnimeList.Data.AnimeDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MyOtakuList",
+        Version = "v1",
+        Description = "An anime, manga and other weeabo shits personal list.", 
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = @"JWT Authorization header using the Bearer scheme." +
+                      "Just enter your token, no need to add 'Bearer' in the beggining."
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>() 
+        }
+    });
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
